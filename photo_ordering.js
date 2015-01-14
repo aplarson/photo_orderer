8a4f01847c5dc043c53157bac2a73b6c96606d48
Andrew P. Larson <andrew.p.larson@gmail.com>
2015-01-13 18:00:02 -0800

    Refactor for more OO solution

--- photo_ordering.js
+++ photo_ordering.js
@@ -1,69 +1,78 @@
 (function () {
   window.Orderable = Orderable = {};
 
-  Orderable.attachListeners = function () {
-    $('.photo').click(window.Orderable.grabElement);
+  Orderable.PhotoArray = PhotoArray =  function () {
+    this.$container = $('.orderable');
   };
 
-  Orderable.grabElement = function (event) {
-    var $el = $(event.target);
-    var position = $el.position();
-    $el.addClass('grabbed');
-    $el.addClass('moved');
-    $el.css('top', event.pageY - 150);
-    $el.css('left', event.pageX - 100);
-    $el.unbind();
-    $el.mousemove(Orderable.moveElement);
-    $el.click(Orderable.releaseElement);
+  Orderable.initialize = function () {
+    var orderable = new PhotoArray();
+    orderable.attachListeners();
   };
 
-  Orderable.moveElement = function (event) {
-    var $el = $(event.target);
-    $el.unbind('mousemove');
-    $el.css('top', event.pageY - 150);
-    $el.css('left', event.pageX - 100);
-    $el.mousemove(Orderable.moveElement);
+  PhotoArray.prototype.attachListeners = function () {
+    this.$container.find('.photo').click(this.grabElement.bind(this));
   };
 
-  Orderable.releaseElement = function (event) {
-    var $el = $(event.target);
-    var draggedPosition = $el.position();
-    if (draggedPosition.top < 300) {
-      var row = 1;
-    } else {
-      var row = 2;
+  PhotoArray.prototype.grabElement = function (event) {
+    this.$el = $(event.target).addClass('grabbed');
+    placeEl(this.$el, event.pageX - 100, event.pageY - 150);
+    this.$el.mousemove(this.moveElement.bind(this));
+    this.$el.click(this.releaseElement.bind(this));
+  };
+
+  PhotoArray.prototype.moveElement = function (event) {
+    this.$el.unbind('mousemove');
+    placeEl(this.$el, event.pageX - 100, event.pageY - 150);
+    this.$el.mousemove(this.moveElement.bind(this));
+  };
+
+  PhotoArray.prototype.placePhotos = function (positions, draggedPos, $photos) {
+    var dropped = false;
+    var draggedLeft = draggedPos.left;
+    $photos.each(function (idx, photo) {
+      var pos = positions[idx];
+      if (!dropped && inRow(draggedPos, pos) && pos.left > draggedLeft) {
+        this.$container.append(this.$el);
+        dropped = true;
+      }
+      this.$container.append($(photo));
+    }.bind(this));
+    if (!dropped) {
+      this.$container.append(this.$el);
     }
-    $el.remove();
-    var $photos = $('.photo');
+  };
+
+  PhotoArray.prototype.releaseElement = function (event) {
+    var draggedPos = this.$el.position();
+    this.$el.remove();
+    var $photos = this.$container.find('.photo');
+    var photoPositions = getPositions($photos);
+    $photos.remove();
+    this.placePhotos(photoPositions, draggedPos, $photos);
+    this.$el.removeClass('grabbed');
+    this.attachListeners();
+  };
+
+  var getPositions = function ($photos) {
     var photoPositions = [];
     $photos.each(function (idx, photo) {
       photoPositions[idx] = $(photo).position();
     });
-    $photos.remove();
-    var dropped = false;
-    var $container = $('.orderable');
-    $photos.each(function (idx, photo) {
-      var $currentPhoto = $(photo);
-      var currentPosition = photoPositions[idx];
-      if (currentPosition.top < 300) {
-        var currentRow = 1;
-      } else {
-        var currentRow = 2;
-      }
-      if (dropped === true || currentRow !== row) {
-        $container.append($currentPhoto);
-      } else if (currentPosition.left < draggedPosition.left) {
-        $container.append($currentPhoto);
-      } else {
-        $container.append($el);
-        dropped = true;
-        $container.append($currentPhoto);
-      }
-      if (!dropped) {
-        $container.append($el);
-      }
-    })
-    $(event.target).removeClass('grabbed');
-    Orderable.attachListeners();
+    return photoPositions;
+  };
+
+  var inRow = function (draggedPos, fixedPos) {
+    if (fixedPos.top < 300) {
+      return draggedPos.top <= 300
+    }
+    else {
+      return draggedPos.top >= 300
+    }
+  };
+
+  var placeEl = function ($el, x, y) {
+    $el.css('top', y);
+    $el.css('left', x);
   };
 })();
